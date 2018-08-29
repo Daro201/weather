b0797600ff9af926c8ab777a7e1c36e759703cdc
Adrian Meller <melleradria@gmail.com>
2018-08-29 15:21:57 +0200

    Add weather.css and update Weather.js

--- src/components/WeatherMap.js
+++ src/components/WeatherMap.js
@@ -4,26 +4,14 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 
 const WeatherMap = ({ latt, long }) => (
-	<div>
-    	<Map google={window.google} zoom={8} 
-    		style={{width: '90%', height: '500px', position: 'relative', padding: '5%', margin: '2%'}}
-    		initialCenter={{
-            	lat: latt,
-            	lng: long
-          	}}
-    		center={{
-            	lat: latt,
-            	lng: long
-          	}}
-        >
- 
-        	<Marker 
-        		position={{lat: latt, lng: long}}
-                name={'Current location'} 
-            />
- 
-      	</Map>
-    </div>
+  <div>
+    <Map google={window.google} zoom={8} 
+    	style={{width: '90%', height: '500px', position: 'relative', padding: '5%', margin: '2%'}}
+    	initialCenter={{ lat: latt, lng: long }}
+    	center={{ lat: latt, lng: long }}>
+      <Marker position={{ lat: latt, lng: long }} name={'Current location'} />
+    </Map>
+  </div>
 );
 
 export default GoogleApiWrapper({
--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -4,6 +4,7 @@ import WeatherForm from '../components/WeatherForm';
 import WeatherMap from '../components/WeatherMap';
 import WeatherTable from '../components/WeatherTable';
 import WeatherHeader from '../components/WeatherHeader';
+import styles from './weather.css';
 
 
 class Weather extends Component {
@@ -17,7 +18,8 @@ class Weather extends Component {
         this.IMAGE_URL = `${this.API_URL}/static/img/weather/`;
 
         this.state = {
-            weatherData: []
+            weatherData: [],
+            woeid: '',
         };
     }
 
@@ -25,22 +27,25 @@ class Weather extends Component {
         fetch(`${this.CORS_URL}${this.API_URL}${this.WOEID_API}${city}`)
             .then(resp => resp.json())
             .then(resp => {
-                fetch(`${this.CORS_URL}${this.API_URL}${this.WEATHER_API}${resp[0].woeid}`)
-                    .then(resp => resp.json())
-                    .then(resp => {
-                        const latt = resp.latt_long.split(',')[0];
-                        const long = resp.latt_long.split(',')[1];
-                        const weatherData = resp.consolidated_weather.map(item => {
-                            return { ...item, weather_state: `${this.IMAGE_URL}${item.weather_state_abbr}.svg` };
-                            }
-                        );
+                if (resp.length && resp[0].woeid) {
+                    this.setState({ woeid: resp[0].woeid });
+                    fetch(`${this.CORS_URL}${this.API_URL}${this.WEATHER_API}${resp[0].woeid}`)
+                        .then(resp => resp.json())
+                        .then(resp => {
+                            const latt = resp.latt_long.split(',')[0];
+                            const long = resp.latt_long.split(',')[1];
+                            const weatherData = resp.consolidated_weather.map(item => {
+                                return { ...item, weather_state: `${this.IMAGE_URL}${item.weather_state_abbr}.svg` };
+                                }
+                            );
 
-                        this.setState({ weatherData });           
-                        this.setState({ latt }); 
-                        this.setState({ long }); 
-                        
-                    });
-
+                            this.setState({ weatherData, latt, long });           
+                           
+                            
+                        });
+                } else {
+                   this.setState({ woeid: null }); 
+                }
             });
     }
 
@@ -50,19 +55,31 @@ class Weather extends Component {
                 <WeatherHeader />
                 <div className="container">                
                     <div className="row">
-                        <div className="col-xs-12">
+                        <div className="col-xs-12" className="text-center">
                             <WeatherForm submitHandler={this.getWeather.bind(this)} />
-                            <div className="col-xs-6">
-                                {this.state.latt !== undefined && 
-                                    <WeatherMap latt={this.state.latt} long={this.state.long} />
-                                }
-                            </div>
-                            <div className="col-xs-6">
-                                {this.state.weatherData.length > 0 && 
-                                    <WeatherTable weatherData={this.state.weatherData} />
-                                }
-                            </div>
-                             
+                            {this.state.woeid !== null ? (
+                                <div>
+                                    <div className="col-xs-6">
+                                        {this.state.latt !== undefined && 
+                                            <WeatherMap latt={this.state.latt} long={this.state.long} />
+                                        }
+                                    </div>
+                                    <div className="col-xs-6">
+                                        {this.state.weatherData.length > 0 && 
+                                            <WeatherTable weatherData={this.state.weatherData} />
+                                        }
+                                    </div>
+                                </div> 
+                            ) : (
+                                <div className="col-xs-12" className ={styles.wather}>
+                                    <div className ="image">  
+                                        <img src="https://images.pexels.com/photos/335393/pexels-photo-335393.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="globe"/>
+                                        <div className="caption post-content">
+                                            <h1>Something went wrong, try again</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
